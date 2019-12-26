abe87f31579e0b777c5ab0722dc41abff41b1484
riskids <dwisriski@yahoo.com>
2019-12-26 13:26:00 +0700

    commit

---
+++ src/rizkybaru/RizkyBaru.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rizkybaru;
+
+/**
+ *
+ * @author Riski Dwi
+ */
+public class RizkyBaru {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
